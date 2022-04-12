8a4c165405c09fd36327ff596d5906c850f1cfe1
AnnSyh <ann.syharevskaya@gmail.com>
2022-04-12 23:33:35 +0300

    add lab block

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,22 @@ import projectsCards from "../utils/projectsCards.js";
 import ProjectsSlider from "./ProjectsSlider.js";
 
 const curentPopupConfirmation = document.querySelector(".confirmation__popup");
+const curentPopupIndustrialLab = document.querySelector(".industrialLab__popup");
 const menuBtn = document.querySelector(".hamburger");
-const subMenu = document.querySelector(".sub-menu");
-const subMenus = document.querySelectorAll(".sub-menu");
+// const labPopup = document.querySelector(".lab-popup");
+const labPopups = document.querySelectorAll(".lab-popup");
 
 const dropdownLi = document.querySelectorAll(".dropdown");
-const dropdownContent = document.querySelectorAll(".dropdown-content");
 
 dropdownLi.forEach(element => {
     element.addEventListener('click', (evt) => {
       const dropdownContentParent = evt.target.parentElement // ищем li именно этой ссылки
-      debugger
       dropdownContentParent.classList.toggle("active");
     });
   });
 
 // -------------------- задаем попап------------------------
-// 1) попап с предупреждением
+// 1) попап с мобильным меню
 const popupConfirmation = new Popup(
   curentPopupConfirmation,
   openConfirm,
@@ -37,15 +36,37 @@ function closeConfirm(evt) {
   popupConfirmation.closePopup(); // <==  закрываем попап ==
 }
 
+// 2) попап с мобильным меню
+const popupIndustrialLab = new Popup(
+  curentPopupIndustrialLab,
+  openIndustrialLab,
+  closeIndustrialLab
+); 
+popupIndustrialLab.setEventListeners(); 
+function openIndustrialLab(evt) {
+  popupIndustrialLab.openPopup(); 
+}
+function closeIndustrialLab(evt) {
+  popupIndustrialLab.closePopup();
+}
+
 // -------------------- /попап------------------------
 
 //подтверждение удаления
 function handleConfirmDelete() {
   popupConfirmation.openPopup(); //открываем окно подтверждения
 }
+function handleIndustrialLabDelete() {
+  popupIndustrialLab.openPopup();
+}
 
-//вешаем событие на кнопки(открывющие попапы с формами)
+//вешаем событие на кнопки(открывющие попапы)
 menuBtn.addEventListener("click", handleConfirmDelete);
+// labPopup.addEventListener("click", handleIndustrialLabDelete);
+
+labPopups.forEach((element) => {
+  element.addEventListener("click", handleIndustrialLabDelete);
+});
 
 const projectSlider = new ProjectsSlider(projectsCards);
 projectSlider.setEventListenersToProjectButtons();
